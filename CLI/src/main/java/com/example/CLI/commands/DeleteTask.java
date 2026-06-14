a937a3b6a31909054bf7f0bfa6116627d44250aa
Patil, Shreyansh <Shreyansh.Patil@wolterskluwer.com>
2026-06-14 20:26:24 +0530

    added createAt and UpdatedAt functions and deleteTask to delete multiple tasks at once

--- CLI/src/main/java/com/example/CLI/commands/DeleteTask.java
+++ CLI/src/main/java/com/example/CLI/commands/DeleteTask.java
@@ -1,5 +1,7 @@
 package com.example.CLI.commands;
 
+import com.example.CLI.commands.helpers.DateAndTime;
+import com.example.CLI.commands.helpers.Id;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -10,19 +12,22 @@ import org.springframework.shell.standard.ShellOption;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 @ShellComponent
 public class DeleteTask {
 
-    final File file = new File("C:\\Users\\Shreyansh.Patil\\IdeaProjects\\Spring-and-Sprinboot\\CLI\\src\\main\\resources\\tasks.json");
+    final File file = new File("C:/Users/Shreyansh.Patil/IdeaProjects/Spring-and-Sprinboot/CLI/src/main/resources/tasks.json");
+    public DateAndTime dnt = new DateAndTime();
+    public Id idHelper =  new Id();
 
-    @ShellMethod(key = "tasks delete", value = "Delete tasks using the name")
-    public String deleteTask(@ShellOption(help = "Task name to delete") String name) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
+    @ShellMethod(key = "tasks delete", value = "Delete task using the id")
+    public String deleteTask(@ShellOption(help = "Task id to delete") Integer id) {
         try{
             if (!file.exists()) {
                 return "Input file not found. No tasks available. To add new tasks please use `tasks-add taskName taskDescription`";
             }
+            ObjectMapper mapper = new ObjectMapper();
             JsonNode rootNode = mapper.readTree(file);
             JsonNode tasksNode = rootNode.get("tasks");
 
@@ -33,25 +38,76 @@ public class DeleteTask {
             ArrayNode newTasksArray = mapper.createArrayNode();
 
             for(JsonNode task : tasksNode){
-                if(!task.get("name").asText().equals(name)){
+                if(!(task.get("id").asInt()==id)){
                     ObjectNode t = mapper.createObjectNode();
-                    t.put("name", task.get("name").asText());
+                    t.put("id", task.get("id").asText());
                     t.put("description", task.get("description").asText());
+                    t.put("status", task.get("status").asText());
+                    t.put("created_at", task.get("created_at").asText());
+                    t.put("updated_at", task.get("updated_at").asText());
 
                     newTasksArray.add(t);
                 }
+            }
+
+            if(newTasksArray.isEmpty()){
+                idHelper.setId(0);
             }
 
             ((ObjectNode) rootNode).set("tasks", newTasksArray);
 
             mapper.writerWithDefaultPrettyPrinter().writeValue(file, rootNode);
 
-            System.out.println("Tasks deleted successfully.");
+            System.out.println("Task deleted successfully.");
         }
         catch(IOException e){
             System.err.println("Error reading or writing JSON: " + e.getMessage());
         }
 
-        return name + " deleted from tasks list";
+        return id + " deleted from tasks list";
+    }
+
+    @ShellMethod(key="tasks delete", value="Delete multiple tasks using ids")
+    public String deleteTask(@ShellOption String[] ids) throws IOException {
+        try{
+            if (!file.exists()) {
+                return "Input file not found. No tasks available. To add new tasks please use `tasks-add taskName taskDescription`";
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(file);
+            JsonNode tasksNode = rootNode.get("tasks");
+
+            if (tasksNode == null || !tasksNode.isArray()) {
+                return "No Tasks in the file to delete";
+            }
+
+            Arrays.sort(ids);
+            int i=0;
+            ArrayNode newTasksArray = mapper.createArrayNode();
+
+            for(JsonNode task : tasksNode){
+                if(i>=ids.length || !(task.get("id").asInt()==Integer.parseInt(ids[i]))){
+                    ObjectNode t = mapper.createObjectNode();
+                    t.put("id", task.get("id").asText());
+                    t.put("description", task.get("description").asText());
+                    t.put("status", task.get("status").asText());
+                    t.put("created_at", task.get("created_at").asText());
+                    t.put("updated_at", task.get("updated_at").asText());
+
+                    newTasksArray.add(t);
+                }
+                else if(task.get("id").asInt()==Integer.parseInt(ids[i])){
+                    i++;
+                }
+            }
+
+            ((ObjectNode) rootNode).set("tasks", newTasksArray);
+            mapper.writerWithDefaultPrettyPrinter().writeValue(file, rootNode);
+        }
+        catch(IOException e){
+            System.err.println("Error reading or writing JSON: " + e.getMessage());
+        }
+
+        return "Tasks deleted successfully";
     }
 }
